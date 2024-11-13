8a19ee3f6437bb78a505a28d4e50cf7d39125f3b
wesley gomes <wesleypgomes2020@gmail.com>
2024-11-13 15:46:48 -0300

    resolvido problema router

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,70 +1,28 @@
 import GlobalStyle from "./styles/global";
-import {toast, ToastContainer} from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import styled from "styled-components"
-//import Form from "./components/Form";
-import Grid from "./components/Grid"
-import Menu from "./components/Menu"
-import axios from "axios"
-import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Consultar from "./pages/Consultar";
 import InserirAlunos from "./pages/InserirAlunos";
 import InserirLivros from "./pages/InserirLivros";
 import NovoEmprestimo from "./pages/NovoEmprestimo";
+import HomePage from "./pages/HomePage";
 
-
-//estilizações
-const Container = styled.div`
-  width: 100%;
-  max-width: 800px;
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-`
-
-const Title = styled.h2``
 
 function App() {
-  const [loans, setLoans] = useState([]);
-  //const [onEdit] = useState(null);
-  //pegando o banco de dados pelo axios
-  const getLoans = async () => {
-    try {
-      const res = await axios.get("http://localhost:8800");
-      setLoans(res.data.sort((a, b) => (a.data_devolucao_prevista > b.data_devolucao_prevista ? 1 : -1)));
-    } catch (error) {
-      toast.error(error);
-    }
-  };
-  //utilizando o banco de dados e colocando no setLoans (eu acho)
-  useEffect(() => {
-    getLoans();
-  }, [setLoans]);
-
 
 // o que vai retornar para o index
   return (
-    <>
+    <Router>
     
-    <Container>      
-      <Title>Teste</Title>
-      <Router>
-      <Menu/>
+   
       <Routes>
+        <Route path = "/" element= {<HomePage/>} />
         <Route path="/Consultar" element={<Consultar />} />
         <Route path="/InserirAlunos" element={<InserirAlunos />} />
         <Route path="/InserirLivros" element={<InserirLivros />} />
         <Route path="/NovoEmprestimo" element={<NovoEmprestimo />} />
       </Routes>
-    </Router>
-      <Grid loans= {loans}/>
-    </Container>
-      <ToastContainer autoClose={3000} position={"bottom-left"}/>
       <GlobalStyle/>
-    </>
+      </Router>
   );
 }
 
---
+++ front_end/src/pages/HomePage.js
@@ -0,0 +1,61 @@
+import GlobalStyle from "../styles/global";
+import {toast, ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import styled from "styled-components"
+//import Form from "./components/Form";
+import Grid from "../components/Grid"
+import Menu from "../components/Menu"
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+
+
+
+//estilizações
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+const Title = styled.h2``
+
+   const HomePage= () =>{
+    const [loans, setLoans] = useState([]);
+    //const [onEdit] = useState(null);
+    //pegando o banco de dados pelo axios
+    const getLoans = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800");
+        setLoans(res.data.sort((a, b) => (a.data_devolucao_prevista > b.data_devolucao_prevista ? 1 : -1)));
+      } catch (error) {
+        toast.error(error);
+      }
+    };
+    //utilizando o banco de dados e colocando no setLoans (eu acho)
+    useEffect(() => {
+      getLoans();
+    },[]);
+  
+  
+
+// o que vai retornar para o index
+  return (
+
+    <>
+    <Container>      
+      <Title>Teste</Title> 
+      <Menu/>
+      <Grid loans= {loans}/>
+    </Container>
+      <ToastContainer autoClose={3000} position={"bottom-left"}/>
+      <GlobalStyle/>
+    </>  
+  );
+}
+
+export default HomePage;
