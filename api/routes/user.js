3e7434dd1a2c1e62557f0a30584af7d1b50496eb
Júlia Nascimento <juliavicnas@gmail.com>
2024-11-27 08:59:38 -0300

    router getAllData

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,11 @@
 import express from "express";
-import  {getBooks, getLoan, alterationSet}  from "../controllers/user.js";
+import  {getBooks, getLoan, alterationSet, getAllData}  from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/", getLoan);
 router.get("/Livros", getBooks)
 router.post("/salvar", alterationSet)
-router.get("/verificador",getAllData)
+router.get("/verificador", getAllData)
 
 export default router;
